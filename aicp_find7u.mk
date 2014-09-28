69617a5b2aafc03aa09c2e959a2a0c1ec5893b83
n3ocort3x <peter.hofinger@gmail.com>
2014-09-28 16:12:41 +0200

    aicp bringup.... again
    Change-Id: I3d2e8c2e2fdef47d75b6ff9e3227c71611d7ba24

--- aicp_find7u.mk
+++ aicp_find7u.mk
@@ -1,19 +1,17 @@
 $(call inherit-product, device/oppo/find7u/full_find7u.mk)
 
-# Get the prebuilt list of APNs
-$(call inherit-product, vendor/nameless/config/apns.mk)
 
 # Inherit some common Nameless stuff
-$(call inherit-product, vendor/nameless/config/common.mk)
+$(call inherit-product, vendor/aicp/configs/common.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/nameless/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 
 # Logo
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/logo.bin:system/logo.bin
 
-PRODUCT_NAME := nameless_find7u
+PRODUCT_NAME := aicp_find7u
 PRODUCT_DEVICE := find7u
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
