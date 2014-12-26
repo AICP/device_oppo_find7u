8725c1ad0523c7ee6f556eac74533e701326c589
Jaime Lopez <jaime.lopez@gmail.com>
2014-12-26 10:32:17 +0100

    Find7u: Bringup with common tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,5 +17,8 @@
 # Inherit from find7-common
 -include device/oppo/find7-common/BoardConfigCommon.mk
 
+# Recovery
+TARGET_RECOVERY_FSTAB := device/oppo/find7u/rootdir/etc/fstab.qcom
+
 # Assert
-TARGET_OTA_ASSERT_DEVICE := find7,find7a,X9007,X9006,FIND7
+TARGET_OTA_ASSERT_DEVICE := find7,find7a,find7u,X9007,X9006,FIND7,FIND7U
