6cfc9be2603d3c7601923f03409763a99673f84b
Jaime Lopez <jaime.lopez@gmail.com>
2014-12-26 10:32:17 +0100

    Find7u: Bringup with common tree
    Change-Id: Ie55c218166491a7dda882aaaf72dce7d462c8bf7

--- find7u.mk
+++ find7u.mk
@@ -21,6 +21,10 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
+# Ramdisk - fstab is different for each variant
+PRODUCT_PACKAGES += \
+    fstab.qcom
+
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
